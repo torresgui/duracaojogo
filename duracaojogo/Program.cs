8bbfa9984defc14fc5e2b4e73fa68049ba370ed9
Guilherme Torres <torrescorreag@gmail.com>
2023-04-05 20:57:33 -0300

    Adicionar arquivos de projeto.

---
+++ duracaojogo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace duracaojogo
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Entrada: ");
+
+            string[] horas = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(horas[0]);
+            int horaFinal = int.Parse(horas[1]);
+
+            int duracao;
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Saída: ");
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
